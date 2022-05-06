bce509a0b58e20879c5df396ad67234e08f5f3b5
User <user@MacBook-Air-3.local>
2022-05-06 09:48:55 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -2,5 +2,7 @@
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネントテスト2
 
-    <x-tests.card title="タイトル" content="本文" />
+    <x-test-class-base classBaseMessage="メッセージです" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
